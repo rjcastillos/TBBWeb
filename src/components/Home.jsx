8d9738a46739ba59230894513557e632785c0da8
Ramon Castillo <ramon@rcastillo.net>
2025-02-11 10:19:19 +0100

    added state but not working properly

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,22 @@
 
 import Recipes from '/Users/ramon/Documents/DocsandMisc/github/projects/TravelByBaking/localworking/allRecipesData.json'
 import Card from './Card'
+import { useState } from 'react'
+
+
+
+
+const handleSearch = (e) => {
+alert(SearchQuery)
+e.preventDefault()
+setSearchQuery("......")
+}
+
+
 const Home = () => {
+
+    const[SearchQuery,setSearchQuery]= useState("");
+
 
     /* {  return(
         <div>
@@ -17,7 +32,24 @@ const Home = () => {
 
         { return (
         
-        <div>   
+        <div className='Home'> 
+
+
+            <form onSubmit={handleSearch} className='search-form'>
+                <input type="text"
+                placeholder='Search for a Recipe...'
+                className='search-input'
+                value={SearchQuery}
+                onChange={(e)=> setSearchQuery(e.target.value)}
+            />
+
+            <button type='Submit' className='search-btn'>
+                Search 
+            </button>
+
+            </form>
+
+        
             {Object.keys(Recipes).map(key =>
 
             <Card Recipe={Recipes[key]} key={Recipes[key]['RecipeCodeName']}/>
