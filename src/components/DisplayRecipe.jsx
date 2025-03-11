f43446c1fcda16f73a5a3095832dbe5ba5f78d5d
Ramon Castillo <ramon@rcastillo.net>
2025-03-11 11:04:58 +0100

    displaying of Arrays within Array and only Array whithin prop implemented , as well as css to the elements so far displayed in the Recipe Details page

--- src/components/DisplayRecipe.jsx
+++ src/components/DisplayRecipe.jsx
@@ -4,13 +4,15 @@ import MyFooter from './MyFooter'
 import Quote from './Quote'
 import MyHeader from './MyHeader'
 import DATASET from '/Users/ramon/Documents/DocsandMisc/github/projects/TravelByBaking/localworking/allRecipesData.json'
-import { h3 } from 'framer-motion/client'
+import { h3, li } from 'framer-motion/client'
 import MyConfig from '../Configuration.json'
 import getRecipesbyCategories from '../getRecipesbyCategory.js'
 import '../styles/DisplayRecipe.css'
 
 const featuredCategories = MyConfig['featuredCategories']
 const fetchedImages =[]
+var A_Ingredients =[]
+var newlist = []
 
 const DisplayRecipe = () => {
 const KEYS = Object.keys(DATASET)
@@ -25,6 +27,13 @@ const { RecipeCodeName } = useParams()
     return fetchedImages
   }
 
+  function getArray (listof){
+      if (Array.isArray(listof[0])) {
+        newlist = listof[0]
+      } else {newlist = listof}
+      return newlist 
+  }
+
   return (
     <div className='Recipe-detail'>
       <MyHeader/>
@@ -33,15 +42,58 @@ const { RecipeCodeName } = useParams()
      <div className='flex flex-col'>
       <h1 className='text-3xl'>{DATASET[RecipeCodeName]['Title']}</h1>
       </div>
-      <div className='flex-col text-sm'>
+      <div className='flex-col text-sm mb-[30px]'>
         <div className='DisplayRecipe_ShortDescription'>
-          <strong>Short Description: </strong><br/> 
-          {DATASET[RecipeCodeName]['ShortDescription']}
+          <ul className='ul_basic'>
+            Short Description: <br/>
+            <li> 
+              {DATASET[RecipeCodeName]['ShortDescription']}
+            </li>
+          </ul>
         </div>
+
+        <div className='DisplayRecipe_Categories'>
+          <ul className='ul_basic'>
+          Categories:<br/> 
+          {getArray(DATASET[RecipeCodeName]['Categories']).map(
+            category =>
+            <li>
+              {category}
+            </li>
+          )}
+          </ul>
+        </div>
+
         <div className='DisplayRecipe_Description'>
           <strong>Description: </strong><br/> 
           {DATASET[RecipeCodeName]['Description']}
         </div>
+        <div className='DisplayRecipe_Ingredients'>
+          <ul>
+   {/*       {`Type ${typeof DATASET[RecipeCodeName]['Ingredients'][0]}  Is it Array? ${Array.isArray(DATASET[RecipeCodeName]['Ingredients'][0])} and length ${DATASET[RecipeCodeName]['Ingredients'][0].length}`} 
+          {Array.isArray(DATASET[RecipeCodeName]['Ingredients'][0]) ? A_Ingredients = DATASET[RecipeCodeName]['Ingredients'][0] : A_Ingredients = DATASET[RecipeCodeName]['Ingredients']}
+         {`Type after ${typeof A_Ingredients}  Is it Array? ${Array.isArray(A_Ingredients)} and length ${A_Ingredients.length}`} 
+           */}
+          {getArray(DATASET[RecipeCodeName]['Ingredients']).map( 
+            ingredient => 
+              <li>
+                {ingredient}
+              </li>
+            )} 
+          </ul> 
+        </div>
+
+        <div className='DisplayRecipe_Directions'>
+          <ul>
+          {getArray(DATASET[RecipeCodeName]['Directions']).map(
+            direction =>
+            <li>
+              {direction}
+            </li>
+          )}
+          </ul>
+        </div>
+
       </div>
       <MyFooter/>
     </div>
